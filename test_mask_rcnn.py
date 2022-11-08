0feb7187a9a03b18079b6d5fbb9864cf1edb6741
zhanglirong1999 <1695074375@qq.com>
2022-11-08 16:41:02 +0800

    second commit

---
+++ test_mask_rcnn.py
@@ -0,0 +1,407 @@
+import tensorflow as tf
+
+import os 
+import sys
+import random
+import math
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import json
+from imgaug import augmenters as iaa
+from tqdm import tqdm
+import pandas as pd 
+import glob 
+
+print(tf.__version__)
+DATA_DIR = '/home/msbd5016/lirong/data'
+# Directory to save logs and trained model
+ROOT_DIR = '/home/msbd5016/lirong'
+# Import Mask RCNN
+sys.path.append(os.path.join(ROOT_DIR, 'Mask_RCNN-master'))  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+from mrcnn.model import log
+debug = False
+train_dicom_dir = os.path.join(DATA_DIR, 'train_v2')
+test_dicom_dir = os.path.join(DATA_DIR, 'test_v2')
+COCO_WEIGHTS_PATH = "mask_rcnn_coco.h5"
+
+import os
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from skimage.io import imread
+import matplotlib.pyplot as plt
+from matplotlib.cm import get_cmap
+from skimage.segmentation import mark_boundaries
+from skimage.util import montage
+from skimage.morphology import binary_opening, disk, label
+import gc; gc.enable() # memory is tight
+from PIL import Image
+from sklearn.model_selection import train_test_split
+
+# The following parameters have been selected to reduce running time for demonstration purposes 
+# These are not optimal 
+
+class DetectorConfig(Config):    
+    # Give the configuration a recognizable name  
+    NAME = 'airbus'
+    
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 9
+    
+    BACKBONE = 'resnet50'
+    
+    NUM_CLASSES = 2  # background and ship classes
+    
+    IMAGE_MIN_DIM = 384
+    IMAGE_MAX_DIM = 384
+    # RPN_ANCHOR_SCALES = (8, 16, 32, 64)
+    TRAIN_ROIS_PER_IMAGE = 64
+    MAX_GT_INSTANCES = 14
+    DETECTION_MAX_INSTANCES = 10
+    DETECTION_MIN_CONFIDENCE = 0.95
+    DETECTION_NMS_THRESHOLD = 0.0
+
+    STEPS_PER_EPOCH = 15 if debug else 150
+    VALIDATION_STEPS = 10 if debug else 125
+    
+    ## balance out losses
+    LOSS_WEIGHTS = {
+        "rpn_class_loss": 30.0,
+        "rpn_bbox_loss": 0.8,
+        "mrcnn_class_loss": 6.0,
+        "mrcnn_bbox_loss": 1.0,
+        "mrcnn_mask_loss": 1.2
+    }
+
+config = DetectorConfig()
+config.display()
+
+
+montage_rgb = lambda x: np.stack([montage(x[:, :, :, i]) for i in range(x.shape[3])], -1)
+
+def multi_rle_encode(img, **kwargs):
+    '''
+    Encode connected regions as separated masks
+    '''
+    labels = label(img)
+    if img.ndim > 2:
+        return [rle_encode(np.sum(labels==k, axis=2), **kwargs) for k in np.unique(labels[labels>0])]
+    else:
+        return [rle_encode(labels==k, **kwargs) for k in np.unique(labels[labels>0])]
+
+# ref: https://www.kaggle.com/paulorzp/run-length-encode-and-decode
+def rle_encode(img, min_max_threshold=1e-3, max_mean_threshold=None):
+    '''
+    img: numpy array, 1 - mask, 0 - background
+    Returns run length as string formated
+    '''
+    if np.max(img) < min_max_threshold:
+        return '' ## no need to encode if it's all zeros
+    if max_mean_threshold and np.mean(img) > max_mean_threshold:
+        return '' ## ignore overfilled mask
+    pixels = img.T.flatten()
+    pixels = np.concatenate([[0], pixels, [0]])
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    runs[1::2] -= runs[::2]
+    return ' '.join(str(x) for x in runs)
+
+def rle_decode(mask_rle, shape=(768, 768)):
+    '''
+    mask_rle: run-length as string formated (start length)
+    shape: (height,width) of array to return 
+    Returns numpy array, 1 - mask, 0 - background
+    '''
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(shape).T  # Needed to align to RLE direction
+
+def masks_as_image(in_mask_list):
+    # Take the individual ship masks and create a single mask array for all ships
+    all_masks = np.zeros((768, 768), dtype = np.uint8)
+    for mask in in_mask_list:
+        if isinstance(mask, str):
+            all_masks |= rle_decode(mask)
+    return all_masks
+
+def masks_as_color(in_mask_list):
+    # Take the individual ship masks and create a color mask array for each ships
+    all_masks = np.zeros((768, 768), dtype = np.float)
+    scale = lambda x: (len(in_mask_list)+x+1) / (len(in_mask_list)*2) ## scale the heatmap image to shift 
+    for i,mask in enumerate(in_mask_list):
+        if isinstance(mask, str):
+            all_masks[:,:] += scale(i) * rle_decode(mask)
+    return all_masks
+
+exclude_list = ['6384c3e78.jpg','13703f040.jpg', '14715c06d.jpg',  '33e0ff2d5.jpg',
+                '4d4e09f2a.jpg', '877691df8.jpg', '8b909bb20.jpg', 'a8d99130e.jpg', 
+                'ad55c3143.jpg', 'c8260c541.jpg', 'd6c7f17c7.jpg', 'dc3e7c901.jpg',
+                'e44dffe88.jpg', 'ef87bad36.jpg', 'f083256d8.jpg'] #corrupted images
+
+train_names = [f for f in os.listdir(train_dicom_dir) if f not in exclude_list]
+test_names = [f for f in os.listdir(test_dicom_dir) if f not in exclude_list]
+
+print(len(train_names), len(test_names))
+SEGMENTATION = DATA_DIR + '/train_ship_segmentations_v2.csv'
+anns = pd.read_csv(SEGMENTATION)
+
+train_names = anns[anns.EncodedPixels.notnull()].ImageId.unique().tolist()  ## override with ships
+
+test_size = config.VALIDATION_STEPS * config.IMAGES_PER_GPU
+image_fps_train, image_fps_val = train_test_split(train_names, test_size=test_size, random_state=42)
+
+# if debug:
+image_fps_train = image_fps_train[:5000]
+image_fps_val = image_fps_val[:1000]
+test_names = test_names[:100]
+    
+print(len(image_fps_train), len(image_fps_val), len(test_names))
+
+class DetectorDataset(utils.Dataset):
+    """Dataset class for training our dataset.
+    """
+
+    def __init__(self, image_fps, image_annotations, orig_height, orig_width):
+        super().__init__(self)
+        
+        # Add classes
+        self.add_class('ship', 1, 'Ship')
+        
+        # add images 
+        for i, fp in enumerate(image_fps):
+            annotations = image_annotations.query('ImageId=="' + fp + '"')['EncodedPixels']
+            self.add_image('ship', image_id=i, path=os.path.join(train_dicom_dir, fp), 
+                           annotations=annotations, orig_height=orig_height, orig_width=orig_width)
+            
+    def image_reference(self, image_id):
+        info = self.image_info[image_id]
+        return info['path']
+
+    def load_image(self, image_id):
+        info = self.image_info[image_id]
+        fp = info['path']
+        image = imread(fp)
+        # If grayscale. Convert to RGB for consistency.
+        if len(image.shape) != 3 or image.shape[2] != 3:
+            image = np.stack((image,) * 3, -1)
+        return image
+
+    def load_mask(self, image_id):
+        info = self.image_info[image_id]
+        annotations = info['annotations']
+#         print(image_id, annotations)
+        count = len(annotations)
+        if count == 0:
+            mask = np.zeros((info['orig_height'], info['orig_width'], 1), dtype=np.uint8)
+            class_ids = np.zeros((1,), dtype=np.int32)
+        else:
+            mask = np.zeros((info['orig_height'], info['orig_width'], count), dtype=np.uint8)
+            class_ids = np.zeros((count,), dtype=np.int32)
+            for i, a in enumerate(annotations):
+                mask[:, :, i] = rle_decode(a)
+                class_ids[i] = 1
+        return mask.astype(np.bool), class_ids.astype(np.int32)
+    
+image_fps, image_annotations = train_names, anns
+ds = imread(os.path.join(train_dicom_dir, image_fps[0])) # read  image from filepath 
+_ = plt.imshow(ds)
+
+# Original image size: 768 x 768
+ORIG_SIZE = ds.shape[0]
+ORIG_SIZE
+
+# prepare the training dataset
+dataset_train = DetectorDataset(image_fps_train, image_annotations, ORIG_SIZE, ORIG_SIZE)
+dataset_train.prepare()
+
+dataset_val = DetectorDataset(image_fps_val, image_annotations, ORIG_SIZE, ORIG_SIZE)
+dataset_val.prepare()
+
+class_ids = [0]
+while class_ids[0] == 0:  ## look for a mask
+    image_id = random.choice(dataset_val.image_ids)
+    image_fp = dataset_val.image_reference(image_id)
+    image = dataset_val.load_image(image_id)
+    mask, class_ids = dataset_val.load_mask(image_id)
+
+masked = np.zeros(image.shape[:2])
+for i in range(mask.shape[2]):
+    masked += mask[:, :, i] ## * image[:, :, 0]
+
+# Image augmentation (light but constant)
+augmentation = iaa.Sequential([
+    iaa.OneOf([ ## rotate
+        iaa.Affine(rotate=0),
+        iaa.Affine(rotate=90),
+        iaa.Affine(rotate=180),
+        iaa.Affine(rotate=270),
+    ]),
+    iaa.Fliplr(0.5),
+    iaa.Flipud(0.5),
+    iaa.OneOf([ ## brightness or contrast
+        iaa.Multiply((0.9, 1.1)),
+        iaa.ContrastNormalization((0.9, 1.1)),
+    ]),
+    iaa.OneOf([ ## blur or sharpen
+        iaa.GaussianBlur(sigma=(0.0, 0.1)),
+        iaa.Sharpen(alpha=(0.0, 0.1)),
+    ]),
+])
+
+# test on the same image as above
+imggrid = augmentation.draw_grid(image, cols=5, rows=2)
+
+model = modellib.MaskRCNN(mode='training', config=config, model_dir=ROOT_DIR)
+
+# Exclude the last layers because they require a matching
+# number of classes
+model.load_weights(COCO_WEIGHTS_PATH, by_name=True, exclude=[
+    "mrcnn_class_logits", "mrcnn_bbox_fc",
+    "mrcnn_bbox", "mrcnn_mask"])
+
+
+LEARNING_RATE = 0.003
+
+# Train Mask-RCNN Model 
+import warnings 
+warnings.filterwarnings("ignore")
+
+model.train(dataset_train, dataset_val,
+            learning_rate=LEARNING_RATE*2,
+            epochs=3,
+            layers='heads',
+            augmentation=None)  ## no need to augment yet
+
+history = model.keras_model.history.history
+
+model.train(dataset_train, dataset_val,
+            learning_rate=LEARNING_RATE,
+            epochs=2 if debug else 10,
+            layers='all',
+            augmentation=augmentation)
+
+new_history = model.keras_model.history.history
+for k in new_history: history[k] = history[k] + new_history[k]
+
+epochs = range(1, len(history['loss'])+1)
+df = pd.DataFrame(history, index=epochs)
+df.to_csv('history.csv')
+
+best_epoch = np.argmin(history["val_loss"])
+score = history["val_loss"][best_epoch]
+print(f'Best Epoch:{best_epoch+1} val_loss:{score}')
+
+# # select trained model 
+# dir_names = next(os.walk(model.model_dir))[1]
+# key = config.NAME.lower()
+# dir_names = filter(lambda f: f.startswith(key), dir_names)
+# dir_names = sorted(dir_names)
+
+# if not dir_names:
+#     import errno
+#     raise FileNotFoundError(
+#         errno.ENOENT,
+#         "Could not find model directory under {}".format(model.model_dir))
+
+# fps = []
+# # Pick last directory
+# for d in dir_names: 
+#     dir_name = os.path.join(model.model_dir, d)
+#     # Find the last checkpoint
+#     checkpoints = next(os.walk(dir_name))[2]
+#     checkpoints = filter(lambda f: f.startswith("mask_rcnn"), checkpoints)
+#     checkpoints = sorted(checkpoints)
+#     if not checkpoints:
+#         print('No weight files in {}'.format(dir_name))
+#     else:
+#         checkpoint = os.path.join(dir_name, checkpoints[best_epoch])
+#         fps.append(checkpoint)
+
+# model_path = sorted(fps)[-1]
+# print('Found model {}'.format(model_path))
+# model_path = '/home/msbd5016/lirong/airbus20221108T0510/mask_rcnn_airbus_0004.h5'
+
+# class InferenceConfig(DetectorConfig):
+#     GPU_COUNT = 1
+#     IMAGES_PER_GPU = 1
+
+# inference_config = InferenceConfig()
+
+# # Recreate the model in inference mode
+# model = modellib.MaskRCNN(mode='inference', 
+#                           config=inference_config,
+#                           model_dir=ROOT_DIR)
+
+# # Load trained weights (fill in path to trained weights here)
+# assert model_path != "", "Provide path to trained weights"
+# print("Loading weights from ", model_path)
+# model.load_weights(model_path, by_name=True)
+
+# # Get filenames of test dataset images
+# test_image_fps = test_names
+
+# DETECTION_TEST_PRED = './ship_detection.csv'
+# ship_detection = pd.read_csv(DETECTION_TEST_PRED, index_col='id')
+# THRESHOLD = 0.45
+# test_names_nothing = ship_detection.loc[ship_detection['p_ship'] <= THRESHOLD].index.tolist()
+# len(test_names_nothing), len(ship_detection), len(test_names_nothing)/len(ship_detection)
+
+
+# # Make predictions on test images, write out sample submission
+# def predict(image_fps, filepath='submission.csv', min_conf=config.DETECTION_MIN_CONFIDENCE):
+#     # assume square image
+#     resize_factor = ORIG_SIZE / config.IMAGE_SHAPE[0]
+#     #resize_factor = ORIG_SIZE
+#     with open(filepath, 'w') as file:
+#         file.write("ImageId,EncodedPixels\n")
+
+#         for image_id in tqdm(image_fps):
+#             found = False
+            
+#             if image_id not in test_names_nothing:
+#                 image = imread(os.path.join(test_dicom_dir, image_id))
+#                 # If grayscale. Convert to RGB for consistency.
+#                 if len(image.shape) != 3 or image.shape[2] != 3:
+#                     image = np.stack((image,) * 3, -1)
+# #                 image, window, scale, padding, crop = utils.resize_image(
+# #                     image,
+# #                     min_dim=config.IMAGE_MIN_DIM,
+# #                     min_scale=config.IMAGE_MIN_SCALE,
+# #                     max_dim=config.IMAGE_MAX_DIM,
+# #                     mode=config.IMAGE_RESIZE_MODE)
+
+#                 results = model.detect([image])
+#                 r = results[0]
+
+#                 assert( len(r['rois']) == len(r['class_ids']) == len(r['scores']) )
+#                 if len(r['rois']) == 0:
+#                     pass  ## no ship
+#                 else:
+#                     num_instances = len(r['rois'])
+
+#                     for i in range(num_instances):
+#                         if r['scores'][i] > min_conf:
+# #                             print(r['scores'][i], r['rois'][i], r['masks'].shape, np.sum(r['masks'][...,i]))
+# #                             plt.imshow(r['masks'][...,i], cmap=get_cmap('jet'))
+#                             file.write(image_id + "," + rle_encode(r['masks'][...,i]) + "\n")
+#                             found = True
+
+#             if not found:
+#                 file.write(image_id + ",\n")  ## no ship
+
+# submission_fp = os.path.join(ROOT_DIR, 'submission.csv')
+# predict(test_image_fps, filepath=submission_fp)
+# print(submission_fp)
+
+# sub = pd.read_csv(submission_fp)
+# print(sub.EncodedPixels.isnull().sum(), sub.ImageId.nunique(), sub.EncodedPixels.isnull().sum()/sub.ImageId.nunique())
+
+
